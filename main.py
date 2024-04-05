d597ec6c3388ddd6b76c91c3b56af0ba0e90b28f
gabriel.erwhian <gabriel.erwhian@ui.ac.id>
2024-04-05 10:23:04 +0700

    feat: added documentation

--- main.py
+++ main.py
@@ -5,7 +5,27 @@ from router import user as user_router, tracked_coin as coin_router
 
 Base.metadata.create_all(bind=db_engine)
 
-app = FastAPI()
+app = FastAPI(
+    description='''
+    API to Track user coin based on Coincap v2
+    
+    ## Users
+    Handle the User of System
+    
+    ## Tracked Coins
+    Handle Tracking Coin for User
+    ''',
+    openapi_tags=[
+        {
+            'name' : 'users',
+            'description' : 'Consist of APIs related to user operation.'
+        },
+        {
+            'name' : 'tracked_coin',
+            'description': 'Handle Tracked Coin Operation. Login first to use these APIs'
+        }
+    ]
+)
 
 
 app.include_router(user_router.router)
